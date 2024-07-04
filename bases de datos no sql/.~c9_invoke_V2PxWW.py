ea1d24d33f635fc46837ab6770020a546f111280
EC2 Default User <ec2-user@ip-172-31-29-2.ec2.internal>
2024-07-04 06:25:00 +0000

    Mi primer cambio en el Repo

---
+++ bases de datos no sql/.~c9_invoke_V2PxWW.py
@@ -0,0 +1,41 @@
+import boto3
+
+#crear cliente para dynamodb
+dynamodb = boto3.resource('dynamodb', region_name = 'us-east-1')
+
+tabla = dynamodb.Table('tabla_maira_cabezas')
+
+#leer un elemento de la tabla
+response = tabla.get_item(Key={'id': '2'})
+
+print(response['Item'])
+
+#Leer todos los elementos de la tabla
+response = tabla.scan()
+
+print(response['Items'])
+
+
+tabla.put_item(Item={
+    "id":"3",
+    "nombre": "Maira Alejandra",
+    "ciudad": "Medellin",
+    "edad":32
+})
+
+#para Actualizar algun valor
+response = tabla.get_item(Key={'id': '3'})
+
+print(response['Item'])
+
+response = table.update_item(
+    Key={
+        'id':'3'
+    },
+    updateExpression='SET edad = :val1, atributo2 = :val2', #Expresion de actualizacion
+    ExpressionAtributeValues={
+        ':val1': '33', # Nuevo valor para atributo 1
+        
+    })
+   
+    
